3098f324d243a6bae383348cd36a7f4708d4efa2
John Carney <jaspajjr@gmail.com>
2017-09-12 02:10:20 +0100

    At this point I want it to be working, but we're getting a Runtime Error: Initialization failed issue. When creating the tsRegression class.

--- cassandra/main.py
+++ cassandra/main.py
@@ -1,9 +1,15 @@
+import pandas as pd
 from forecast import tsRegression
+from utils import cv_rmse
 
 
 def main(df):
-    return 0
+    ts = tsRegression()
+    score = cv_rmse(ts, df)
+    return score
 
 
 if __name__ == '__main__':
-    main()
+    file_loc = '~/working/github/cassandra/data/retail.csv'
+    df = pd.read_csv(file_loc)
+    score = main(df)
